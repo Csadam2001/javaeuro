11ffc1a38e6982b990abde36f3fb0c8df30f5555
Csadam2001 <csizmadiaadam1@gmail.com>
2022-08-29 13:45:56 +0200

    test

--- app/src/test/java/javaeuro/AppTest.java
+++ app/src/test/java/javaeuro/AppTest.java
@@ -3,9 +3,12 @@
  */
 package javaeuro;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
 class AppTest {
     @Test void appHasAGreeting() {
+        assertNotNull(App.getapi(), "Error!");
     }
 }
